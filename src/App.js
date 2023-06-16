68014cd6354f2a2a73d7c838b28e0d926ee907bd
Anel Canto <ac@anelcanto.com>
2023-06-15 17:02:14 -1000

    Add task button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+
 
 class App extends Component {
   constructor(props) {
@@ -20,28 +22,42 @@ class App extends Component {
     });
   }
 
+  addItem() {
+    if (this.state.userInput !== "") {
+      const userInput = {
+        // Add a random id which is used to delete
+        id: Math.random(),
+
+        // Add a user value to list
+        value: this.state.userInput,
+      };
+
+      // Update list
+      const list = [...this.state.list];
+      list.push(userInput);
+
+      // reset state
+      this.setState({
+        list,
+        userInput: "",
+      });
+    }
+  }
+
 
   render() {
-
-    // In render method
-    <FormControl
-      placeholder="add item . . . "
-      size="lg"
-      value={this.state.userInput}
-      onChange={(item) => this.updateInput(item.target.value)}
-      aria-label="add something"
-      aria-describedby="basic-addon2"
-    />
-
     return (
-      <FormControl
-        placeholder="add item . . . "
-        size="lg"
-        value={this.state.userInput}
-        onChange={(item) => this.updateInput(item.target.value)}
-        aria-label="add something"
-        aria-describedby="basic-addon2"
-      />
+      <>
+        <FormControl
+          placeholder="add item . . . "
+          size="lg"
+          value={this.state.userInput}
+          onChange={(item) => this.updateInput(item.target.value)}
+          aria-label="add something"
+          aria-describedby="basic-addon2"
+        />
+        <Button variant="dark" className="mt-2" onClick={() => this.addItem()}>ADD</Button>
+      </>
     );
   }
 }
