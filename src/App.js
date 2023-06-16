ebe44e9aa80b1aa8f008e4d343f2d2ce5c66fcaa
Anel Canto <ac@anelcanto.com>
2023-06-15 16:47:17 -1000

    Task template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // Setting up state
+    this.state = {
+      userInput: "",
+      list: [],
+    };
+  }
+
+  render() {
+    return (<div>Task</div>);
+  }
 }
 
 export default App;
