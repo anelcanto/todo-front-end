7500d48028e7950a35202aa971364ded11354311
Anel Canto <ac@anelcanto.com>
2023-06-16 18:30:09 -1000

    Add Bootstrap input group and Col items

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import Col from "react-bootstrap/Col";
 
 
 class App extends Component {
@@ -32,6 +34,8 @@ class App extends Component {
         value: this.state.userInput,
       };
 
+      console.log(userInput)
+
       // Update list
       const list = [...this.state.list];
       list.push(userInput);
@@ -44,6 +48,19 @@ class App extends Component {
     }
   }
 
+  // Function to delete item from list use id to delete
+  deleteItem(key) {
+    const list = [...this.state.list];
+
+    // Filter values and leave value which we need to delete
+    const updateList = list.filter((item) => item.id !== key);
+
+    // Update list in state
+    this.setState({
+      list: updateList,
+    });
+  }
+
 
   render() {
     return (
@@ -52,11 +69,20 @@ class App extends Component {
           placeholder="add item . . . "
           size="lg"
           value={this.state.userInput}
-          onChange={(item) => this.updateInput(item.target.value)}
+          onChange={
+            (item) => this.updateInput(item.target.value)
+          }
           aria-label="add something"
           aria-describedby="basic-addon2"
         />
-        <Button variant="dark" className="mt-2" onClick={() => this.addItem()}>ADD</Button>
+        <Col md={{ span: 5, offset: 4 }}>
+          <InputGroup>
+            <InputGroup>
+
+              <Button variant="dark" className="mt-2" onClick={() => this.addItem()}>ADD</Button>
+            </InputGroup>
+          </InputGroup>
+        </Col>
       </>
     );
   }
