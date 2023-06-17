170103bc32195aaaecd0382781a3ba35e5df313f
Anel Canto <ac@anelcanto.com>
2023-06-16 22:02:19 -1000

    Add container & listgroup

--- src/App.js
+++ src/App.js
@@ -66,43 +66,79 @@ class App extends Component {
   render() {
     return (
       <>
-        <Row
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            fontSize: "3rem",
-            fontWeight: "bolder",
-          }}
-        >
-          TODO LIST
-        </Row>
-        <hr />
-        <Row>
-          <Col md={{ span: 5, offset: 4 }}>
-            <InputGroup className="mb-3">
-              <FormControl
-                placeholder="add item . . . "
-                size="lg"
-                value={this.state.userInput}
-                onChange={(item) =>
-                  this.updateInput(item.target.value)
-                }
-                aria-label="add something"
-                aria-describedby="basic-addon2"
-              />
-              <InputGroup>
-                <Button
-                  variant="dark"
-                  className="mt-2"
-                  onClick={() => this.addItem()}
-                >
-                  ADD
-                </Button>
+        <Container>
+          <Row
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              fontSize: "3rem",
+              fontWeight: "bolder",
+            }}
+          >
+            TODO LIST
+          </Row>
+
+          <hr />
+          <Row>
+            <Col md={{ span: 5, offset: 4 }}>
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="add item . . . "
+                  size="lg"
+                  value={this.state.userInput}
+                  onChange={(item) =>
+                    this.updateInput(item.target.value)
+                  }
+                  aria-label="add something"
+                  aria-describedby="basic-addon2"
+                />
+                <InputGroup>
+                  <Button
+                    variant="dark"
+                    className="mt-2"
+                    onClick={() => this.addItem()}
+                  >
+                    ADD
+                  </Button>
+                </InputGroup>
               </InputGroup>
-            </InputGroup>
-          </Col>
-        </Row>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={{ span: 5, offset: 4 }}>
+              <ListGroup>
+                {this.state.list.map((item, index) => {
+                  return (
+                    <div key={index} >
+                      <ListGroup.Item
+                        variant="dark"
+                        action
+                        style={{
+                          display: "flex",
+                          justifyContent: 'space-between'
+                        }}
+                      >
+                        {item.value}
+                        <span>
+                          <Button style={{ marginRight: "10px" }}
+                            variant="light"
+                            onClick={() => this.deleteItem(item.id)}>
+                            Delete
+                          </Button>
+                          <Button variant="light"
+                            onClick={() => this.editItem(index)}>
+                            Edit
+                          </Button>
+                        </span>
+                      </ListGroup.Item>
+                    </div>
+                  );
+                })}
+              </ListGroup>
+            </Col>
+          </Row>
+        </Container>
       </>
     );
   }
